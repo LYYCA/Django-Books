ee73838a8429519b48fbf4ccad6f8aa770b84cd7
Fergus Wedd <wedd.fergus@gmail.com>
2020-04-25 16:53:28 +0800

    Add user permissions

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -5,12 +6,17 @@ from .models import Book
 
 # Create your views here.
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    # Redirects the user to the login page if they're not logged in
+    login_url = 'account_login'
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+    # Redirects the user to the login page if they're not logged in
+    login_url = 'account_login'
+    permission_required = 'books.special_status'
